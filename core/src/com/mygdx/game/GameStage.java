e48bda5e064ff2b6e59c104ca930e5c2ab9eb9a3
Majki94 <master.atwork@yahoo.com>
2016-11-27 20:28:15 +0100

    Dodat pocetni ekran i delimicno uradjen ulazu igru.

---
+++ core/src/com/mygdx/game/GameStage.java
@@ -0,0 +1,114 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+/**
+ * Created by Majki on 27.11.2016.
+ */
+
+public class GameStage extends Stage{
+
+    Objekat lopta;
+    Objekat gameover;
+    Objekat restart;
+    double randomX;
+    double randomY;
+    int min = 10000;
+    int max = 10000;
+
+    public GameStage(){
+        gameover = new Objekat();
+        gameover.setTexture(new Texture("gameover.png"));
+        gameover.setVisible(false);
+        gameover.setWidth(300);
+        gameover.setHeight(100);
+        gameover.setPosition(Gdx.graphics.getWidth() / 2 - gameover.getWidth() / 2, Gdx.graphics.getHeight() / 2 - gameover.getHeight() / 2);
+
+        restart = new Objekat();
+        restart.setTexture(new Texture("restart.png"));
+        restart.setVisible(false);
+        restart.setWidth(300);
+        restart.setHeight(100);
+        restart.setPosition(Gdx.graphics.getWidth() / 2 - restart.getWidth() / 2, 0);
+        restart.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+            }
+        });
+
+        lopta = new Objekat();
+        lopta.setTexture(new Texture("ball2.png"));
+        lopta.setWidth(30);
+        lopta.setHeight(30);
+        lopta.setPosition(Gdx.graphics.getWidth() / 2 - lopta.getWidth() / 2, Gdx.graphics.getHeight() / 2 - lopta.getHeight() / 2);
+        lopta.brzinaX = 50;
+        lopta.brzinaY = 50;
+
+        addActor(lopta);
+        addActor(gameover);
+        addActor(restart);
+    }
+
+    public Objekat getLopta() {
+        return lopta;
+    }
+
+    public void setLopta(Objekat lopta) {
+        this.lopta = lopta;
+    }
+
+    public Objekat getGameover() {
+        return gameover;
+    }
+
+    public void setGameover(Objekat gameover) {
+        this.gameover = gameover;
+    }
+
+    public Objekat getRestart() {
+        return restart;
+    }
+
+    public void setRestart(Objekat restart) {
+        this.restart = restart;
+    }
+
+    public double getRandomX() {
+        return randomX;
+    }
+
+    public void setRandomX(double randomX) {
+        this.randomX = randomX;
+    }
+
+    public double getRandomY() {
+        return randomY;
+    }
+
+    public void setRandomY(double randomY) {
+        this.randomY = randomY;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+
+}
---
+++ core/src/com/mygdx/game/StartStage.java
@@ -0,0 +1,100 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+/**
+ * Created by Majki  on 27.11.2016.
+ */
+
+public class StartStage extends Stage{
+
+    Objekat start;
+    Objekat settings;
+    Objekat exit;
+    boolean startClicked = false;
+
+    public StartStage(){
+
+        start = new Objekat();
+        start.setTexture(new Texture("start.png"));
+        start.setVisible(true);
+        start.setWidth(250);
+        start.setHeight(100);
+        start.setPosition(Gdx.graphics.getWidth() / 2 - start.getWidth() / 2, Gdx.graphics.getHeight() / 2 - start.getHeight() / 2 + 150);
+        start.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                startClicked = true;
+            }
+        });
+
+        settings = new Objekat();
+        settings.setTexture(new Texture("settings.png"));
+        settings.setVisible(true);
+        settings.setWidth(200);
+        settings.setHeight(70);
+        settings.setPosition(Gdx.graphics.getWidth() / 2 - settings.getWidth() / 2, Gdx.graphics.getHeight() / 2 - settings.getHeight() / 2);
+//        settings.addListener(new ClickListener(){
+//            @Override
+//            public void clicked(InputEvent event, float x, float y) {
+//                super.clicked(event, x, y);
+//                new MyGdxGame();
+//            }
+//        });
+
+        exit = new Objekat();
+        exit.setTexture(new Texture("exit.png"));
+        exit.setVisible(true);
+        exit.setWidth(100);
+        exit.setHeight(50);
+        exit.setPosition(Gdx.graphics.getWidth() / 2 - exit.getWidth() / 2, Gdx.graphics.getHeight() / 2 - exit.getHeight() / 2 -  100);
+//        exit.addListener(new ClickListener(){
+//            @Override
+//            public void clicked(InputEvent event, float x, float y) {
+//                super.clicked(event, x, y);
+//                dispose();
+//            }
+//        });
+
+        addActor(start);
+        addActor(settings);
+        addActor(exit);
+    }
+
+    public Objekat getStart() {
+        return start;
+    }
+
+    public void setStart(Objekat start) {
+        this.start = start;
+    }
+
+    public Objekat getSettings() {
+        return settings;
+    }
+
+    public void setSettings(Objekat settings) {
+        this.settings = settings;
+    }
+
+    public Objekat getExit() {
+        return exit;
+    }
+
+    public void setExit(Objekat exit) {
+        this.exit = exit;
+    }
+
+    public boolean isStartClicked() {
+        return startClicked;
+    }
+
+    public void setStartClicked(boolean startClicked) {
+        this.startClicked = startClicked;
+    }
+}
